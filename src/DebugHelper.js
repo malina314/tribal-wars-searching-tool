dd99f8d6fcf22d15a0ae9079bb39ed40260abf9e
Mateusz Malinowski <malina314@op.pl>
2022-01-23 22:11:30 +0100

    complete inserting data

--- src/DebugHelper.js
+++ src/DebugHelper.js
@@ -1,8 +1,10 @@
+const logError = require("./logError");
+
 module.exports = {
     logQuery: ({text, values}) => {
         const short = true;
         if (short) {
-            console.log(text.slice(0, 300));
+            console.log(text.slice(0, 160));
             // console.log(values.slice(0, 20));
         } else {
             console.log(text);
@@ -11,6 +13,11 @@ module.exports = {
     },
 
     logRes: (res) => {
+        if (typeof res === 'undefined') {
+            logError('res is undefined');
+            return;
+        }
+
         switch (res.command) {
             case 'INSERT':
                 console.log(`INSERTED ${res.rowCount} rows.`);
--- src/repository/Postgres.js
+++ src/repository/Postgres.js
@@ -1,18 +1,12 @@
 const getData = require('../getData');
-const { Pool, Client } = require('pg');
+const { Pool } = require('pg');
 const decodeUriComponent = require('decode-uri-component');
 const logError = require('../logError');
 const DebugHelper = require('../DebugHelper');
 
 module.exports = class Postgres {
     constructor() {
-        this.type = {
-            TRIBES: 'tribes',
-            PLAYERS: 'players',
-            VILLAGES: 'villages',
-        }
-
-        this.tables = {};
+        this.servers = {};
 
         this.pool = new Pool({
             user: process.env.user,
@@ -22,127 +16,186 @@ module.exports = class Postgres {
 
     // używanie zmiennej server jest bezpieczne bo jest walidowana wcześniej i spełnia regex /p?[1-9]\d*/
     async updateServerData(server) {
-        if (!this.tables[server]) {
+        if (!this.servers[server]) {
             await this.updateTribes(server);
             await this.updatePlayers(server);
             await this.updateVillages(server);
-            this.tables[server] = true;
+            this.servers[server] = true;
         }
     }
     
     async getTribes(server) {
         await this.updateServerData(server);
-        return this.tables[server];
+        return this.servers[server];
     }
     
     async getPlayers(server) {
         await this.updateServerData(server);
-        return this.tables[server].players;
+        return this.servers[server].players;
     }
     
     async getVillages(server) {
         await this.updateServerData(server);
-        return this.tables[server].villages;
+        return this.servers[server].villages;
     }
 
-    async updateQuery(server, type, data) {
-        const tableName = `server_${server}_${type}`;
+    makeValueString(rows, arity) {
+        let result = [];
 
-        function makeValueString() {
-            let result = [];
-
-            for (let i = 0; i < data.rows; i++) {
-                const line = [];
-                for (let j = 1; j <= data.arity; j++) {
-                    line.push(`$${i * data.arity + j}`);
-                }
-                result.push(`(${line.join(',')})`);
+        for (let i = 0; i < rows; i++) {
+            const line = [];
+            for (let j = 1; j <= arity; j++) {
+                line.push(`$${i * arity + j}`);
             }
-
-            return result.join(',');
+            result.push(`(${line.join(',')})`);
         }
 
-        // todo: DELETE *
-        await this.pool.query(`
-            ALTER TABLE IF EXISTS server_${server}_players
-            DROP CONSTRAINT IF EXISTS fk_constraint;
-        `)
-        .catch(logError);
-        
-        await this.pool.query(`
-            DROP TABLE IF EXISTS ${tableName};
-        `)
-        .catch(logError);
+        return result.join(',');
+    }
 
-        switch (type) {
-            case this.type.TRIBES:
-                await this.pool.query(`CREATE TABLE ${tableName} (
-                    id INTEGER PRIMARY KEY,
-                    full_name VARCHAR(255) UNIQUE NOT NULL,
-                    name VARCHAR(15) UNIQUE NOT NULL,
-                    members_cnt INTEGER NOT NULL,
-                    villages_cnt INTEGER NOT NULL,
-                    top_40_points INTEGER NOT NULL,
-                    points INTEGER NOT NULL,
-                    rank INTEGER UNIQUE NOT NULL
-                );`)
-                .catch(logError);
-
-                const query = {
-                    text: `INSERT INTO ${tableName} (id, full_name, name, members_cnt, villages_cnt, top_40_points, points, rank)
-                        VALUES ${makeValueString()};`,
-                    values: data.array,
-                }
-
-                DebugHelper.logQuery(query);
-
-                const res = await this.pool.query(query)
-                .catch(logError);
-
-                DebugHelper.logRes(res);
-
-                break;
-            case this.type.PLAYERS:
-                // todo
-                break;
-                case this.type.VILLAGES:
-                // todo
-                break;
-        }
+    dataToArray(data) {
+        return data.split(/\n(?=.)|,/gm).map(el => decodeUriComponent(el));
     }
 
     async updateTribes(server) {
-        const rawTribes = await getData(`https://pl${server}.plemiona.pl/map/ally.txt`);
-
-        const array = rawTribes.split(/\n(?=.)|,/gm).map(el => this.decodeRawText(el));
-        const arity = 8; // id, fullName, name, membersCnt, villagesCnt, top40points, points, rank
-        const rows = array.length / arity;
-        
-        await this.updateQuery(server, this.type.TRIBES, {array, arity, rows});
+        await this.updateTribesQuery(server, {
+            array: this.dataToArray(await getData(`https://pl${server}.plemiona.pl/map/ally.txt`)), 
+            arity: 8, // id, full_name, name, members_cnt, villages_cnt, top_40_points, points, rank
+        });
     }
 
     async updatePlayers(server) {
-        const rawPlayers = await getData(`https://pl${server}.plemiona.pl/map/player.txt`);
-        
-        const array = rawPlayers.split(/\n(?=.)|,/gm).map(el => this.decodeRawText(el));
-        const arity = 6; // id, name, tribeId, villagesCnt, points, rank
-        const rows = array.length / arity;
-        
-        await this.updateQuery(server, this.type.PLAYERS, {array, arity, rows});
+        const array = this.dataToArray(await getData(`https://pl${server}.plemiona.pl/map/player.txt`));
+        const arity = 6; // id, name, tribe_id, villages_cnt, points, rank
+
+        for (let i = 2; i < array.length; i += arity) {
+            if (array[i] == 0) {
+                array[i] = null;
+            }
+        }
+
+        await this.updatePlayersQuery(server, {array, arity});
     }
 
     async updateVillages(server) {
-        const rawVillages = await getData(`https://pl${server}.plemiona.pl/map/village.txt`);
-        
-        const array = rawVillages.split(/\n(?=.)|,/gm).map(el => this.decodeRawText(el));
-        const arity = 7; // id, name, x, y, playerId, points, bonus
-        const rows = array.length / arity;
-        
-        await this.updateQuery(server, this.type.VILLAGES, {array, arity, rows});
+        const array = this.dataToArray(await getData(`https://pl${server}.plemiona.pl/map/village.txt`));
+        const arity = 7; // id, name, x, y, player_id, points, bonus
+
+        for (let i = 4; i < array.length; i += arity) {
+            if (array[i] == 0) {
+                array[i] = null;
+            }
+        }
+
+        await this.updateVillagesQuery(server, {array, arity});
     }
 
-    //todo: zrobić z tego dataToArray
-    decodeRawText(text) {
-        return decodeUriComponent(text);
+    async updateTribesQuery(server, data) {
+        const tableName = `server_${server}_tribes`;
+        
+        await this.pool.query(`CREATE TABLE IF NOT EXISTS ${tableName} (
+            id INTEGER PRIMARY KEY,
+            full_name VARCHAR(255) UNIQUE NOT NULL,
+            name VARCHAR(15) UNIQUE NOT NULL,
+            members_cnt INTEGER NOT NULL,
+            villages_cnt INTEGER NOT NULL,
+            top_40_points INTEGER NOT NULL,
+            points INTEGER NOT NULL,
+            rank INTEGER UNIQUE NOT NULL
+        );`)
+        .catch(logError);
+
+        await this.pool.query(`TRUNCATE TABLE ${tableName} CASCADE;`)
+        .catch(logError);
+
+        const query = {
+            text: `INSERT INTO ${tableName} (id, full_name, name, members_cnt, villages_cnt, top_40_points, points, rank)
+                VALUES ${this.makeValueString(data.array.length / data.arity, data.arity)};`,
+            values: data.array,
+        }
+
+        DebugHelper.logQuery(query);
+
+        const res = await this.pool.query(query)
+        .catch(logError);
+
+        DebugHelper.logRes(res);
+    }
+
+    async updatePlayersQuery(server, data) {
+        const tableName = `server_${server}_players`;
+        
+        await this.pool.query(`CREATE TABLE IF NOT EXISTS ${tableName} (
+            id INTEGER PRIMARY KEY,
+            name VARCHAR(255) UNIQUE NOT NULL,
+            tribe_id INTEGER,
+            CONSTRAINT fk_constraint
+                FOREIGN KEY (tribe_id)
+                    REFERENCES server_${server}_tribes(id),
+            villages_cnt INTEGER NOT NULL,
+            points INTEGER NOT NULL,
+            rank INTEGER UNIQUE NOT NULL
+        );`)
+        .catch(logError);
+
+        await this.pool.query(`TRUNCATE TABLE ${tableName} CASCADE;`)
+        .catch(logError);
+
+        const chunkSize = Math.floor(65535 / data.arity) * data.arity;
+        
+        for (let i = 0; i < data.array.length; i += chunkSize) {
+            const query = {
+                text: `INSERT INTO ${tableName} (id, name, tribe_id, villages_cnt, points, rank)
+                    VALUES ${this.makeValueString(data.array.length / data.arity, data.arity)};`,
+                values: data.array,
+            }
+
+            DebugHelper.logQuery(query);
+
+            const res = await this.pool.query(query)
+            .catch(logError);
+
+            DebugHelper.logRes(res);
+        }
+    }
+
+    async updateVillagesQuery(server, data) {
+        const tableName = `server_${server}_villages`;
+        
+        await this.pool.query(`CREATE TABLE IF NOT EXISTS ${tableName} (
+            id INTEGER PRIMARY KEY,
+            name VARCHAR(255) NOT NULL,
+            x INTEGER NOT NULL,
+            y INTEGER NOT NULL,
+            player_id INTEGER,
+            CONSTRAINT fk_constraint
+                FOREIGN KEY (player_id)
+                    REFERENCES server_${server}_players(id),
+            points INTEGER NOT NULL,
+            bonus INTEGER NOT NULL
+        );`)
+        .catch(logError);
+
+        console.log(tableName);
+
+        await this.pool.query(`TRUNCATE TABLE ${tableName} CASCADE;`)
+        .catch(logError);
+
+        const chunkSize = Math.floor(65535 / data.arity) * data.arity;
+        
+        for (let i = 0; i < data.array.length; i += chunkSize) {
+            const query = {
+                text: `INSERT INTO ${tableName} (id, name, x, y, player_id, points, bonus)
+                    VALUES ${this.makeValueString(Math.min(chunkSize, data.array.length - i) / data.arity, data.arity)};`,
+                values: data.array.slice(i, i + chunkSize),
+            }
+    
+            DebugHelper.logQuery(query);
+    
+            const res = await this.pool.query(query)
+            .catch(logError);
+    
+            DebugHelper.logRes(res);
+        }
     }
 }
